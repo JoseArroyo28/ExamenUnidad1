209d74b06d3e83368093d4ec53dfdd431454291d
jose arroyo <pechelon_2@hotmail.com>
2020-10-14 17:28:01 -0700

    Agregar archivos de proyecto.

---
+++ Examen/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Examen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMas = new System.Windows.Forms.Button();
+            this.btnMenos = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnMas
+            // 
+            this.btnMas.Location = new System.Drawing.Point(37, 131);
+            this.btnMas.Name = "btnMas";
+            this.btnMas.Size = new System.Drawing.Size(75, 23);
+            this.btnMas.TabIndex = 0;
+            this.btnMas.Text = "+";
+            this.btnMas.UseVisualStyleBackColor = true;
+            this.btnMas.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnMenos
+            // 
+            this.btnMenos.Location = new System.Drawing.Point(37, 173);
+            this.btnMenos.Name = "btnMenos";
+            this.btnMenos.Size = new System.Drawing.Size(75, 23);
+            this.btnMenos.TabIndex = 1;
+            this.btnMenos.Text = "-";
+            this.btnMenos.UseVisualStyleBackColor = true;
+            this.btnMenos.Click += new System.EventHandler(this.btnMenos_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(118, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(269, 436);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnMenos);
+            this.Controls.Add(this.btnMas);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMas;
+        private System.Windows.Forms.Button btnMenos;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Examen/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int index = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (index <= 25)
+            {
+                ListBox ltv = new ListBox();
+                ltv.Items.Add(" " + index);
+                listBox1.Items.Add(index);
+                index++;
+            }
+            else
+            {
+                MessageBox.Show("Limite de Numeros");
+            }
+            
+        }
+
+        private void btnMenos_Click(object sender, EventArgs e)
+        {
+            if (index > 0)
+            {
+                ListBox ltv = new ListBox();
+                ltv.Items.Add(" " + index);
+                listBox1.Items.RemoveAt(index-1);
+                index--;
+            }
+            else
+            {
+                MessageBox.Show("No hay Numeros");
+            }
+        }
+    }
+}
